d3ab9cb65d4a9fded1ba20499d0494334e5fb98a
Maxim Kuzmenko <mway2914@yandex.ru>
2023-04-08 15:21:05 +0300

    add a project and gitignore

---
+++ SimpleCodeTestTask/Solution/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public static int[] solution(string[] A, string[] B)
+    {
+        HashSet<string> forbidden = new HashSet<string>(B); // хэш-сет запрещенных хостов
+        HashSet<string> allowed = new HashSet<string>(); // хэш-сет доступных хостов
+        Queue<string> queue = new Queue<string>(); // очередь доступных хостов
+
+        foreach (string host in A)
+        { 
+            if (forbidden.Contains(host)) continue; 
+
+            bool isAllowed = true;
+
+            for (int i = host.Length - 1; i >= 0; i--)
+            { 
+                if (host[i] == '.')
+                { 
+                    string parent = host.Substring(0, i); 
+
+                    if (forbidden.Contains(parent))
+                    { 
+                        isAllowed = false; 
+                        break; 
+                    }
+                }
+            }
+
+            if (isAllowed)
+            { 
+                allowed.Add(host); 
+                queue.Enqueue(host); 
+            }
+        }
+
+        int[] result = new int[queue.Count];
+        int index = 0;
+
+        while (queue.Count > 0)
+        {
+            string host = queue.Dequeue();
+            result[index++] = Array.IndexOf(A, host);
+        }
+
+        return result;
+    }
+}
---
+++ SimpleCodeTestTask/Testing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] A = {
+            "unlock.microvirus.md",
+            "visitwar.com",
+            "visitwar.de",
+            "fruonline.co.uk",
+            "australia.open.com",
+            "credit.card.us"
+        };
+        string[] B = {
+            "microvirus.md",
+            "visitwar.de",
+            "piratebay.co.uk",
+            "list.stolen.credit.card.us"
+        };
+
+        int[] result = Solution.solution(A, B);
+
+        Console.Write("Результат: [ ");
+        foreach (int i in result)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine("]");
+    }
+}
